6bc62a6abb59447486e10d163cd9f57513c3bbc2
omarabdelfattah <omar.mero200125@gmail.com>
2022-05-22 01:12:29 +0200

    details page

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app_4/screens/doctor_list.dart';
 
 void main() {
+  Map<int, Color> color =
+  {
+    50: Color(0xff07da5f),
+    100: Color(0xff07da5f),
+    200: Color(0xff07da5f),
+    300: Color(0xff07da5f),
+    400: Color(0xff07da5f),
+    500: Color(0xff07da5f),
+    600: Color(0xff07da5f),
+    700: Color(0xff07da5f),
+    800: Color(0xff07da5f),
+    900: Color(0xff07da5f),
+  };
+  MaterialColor primeColor = MaterialColor(0xff07da5f, color);
+
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: DoctorList(),
       theme: ThemeData(
-        primaryColor: Color(0xff07da5f)
+          primarySwatch: primeColor,
+          appBarTheme: AppBarTheme(
+              foregroundColor: Colors.white
+          ),
       ),
     ),
   );
